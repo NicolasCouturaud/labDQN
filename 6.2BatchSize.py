a79fbc98ef36e8f5a21bc0e3b4d648ab12ebd3d6
nicoc <nicolas.couturaud@edu.ece.fr>
2023-12-05 16:03:10 +0100

    1

---
+++ 6.2BatchSize.py
@@ -0,0 +1,23 @@
+import torch
+import torch.utils.data as Data
+
+torch.manual_seed(1234)  # reproducible
+batch_size = 5
+
+x = torch.linspace(1, 10, 10)  # this is x data (torch tensor)
+y = torch.linspace(10, 1, 10)  # this is y data (torch tensor)
+
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,  # torch TensorDataset format
+    batch_size=batch_size,  # mini batch size
+    shuffle=True,  # random shuffle for training
+    num_workers=2  # subprocesses for loading data
+)
+
+for epoch in range(5):  # train entire dataset 5 times
+# for each training step
+    for step, (batch_x, batch_y) in enumerate(loader):
+        # write your code here to train data in mini-batch
+        print('Epoch: ', epoch, ' | Step: ', step, ' | batch x: ',
+        batch_x.numpy(), ' | batch y: ', batch_y.numpy())
